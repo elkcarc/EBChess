af88484454c3f2e4ba9549ee90eae1443a8bb13a
Peyton Hazama <peytonhazama@gmail.com>
2020-04-06 19:21:03 -0400

    added superuser and db configuration

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    game_players = models.CharField(max_length=200)
+    game_content = models.TextField()
+    game_published = models.DateTimeField("date played")
+
+    def __str__(self):
+        return self.game_players
