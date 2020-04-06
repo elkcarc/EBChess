72eb3762df35c47e16f26c6cff26050704a70eba
Peyton Hazama <peytonhazama@gmail.com>
2020-04-06 19:51:39 -0400

    added tinymce extension

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -17,8 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
    url(r'', include('main.urls')), 
    url(r'^admin/', admin.site.urls),
+   path('tinymce/', include('tinymce.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Game
-
+from tinymce.widgets import TinyMCE
+from django.db import models
 
 # Register your models here.
 
-admin.site.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Title/date", {"fields":["game_players", "game_published"]}),
+        ("Content", {"fields":["game_content"]})
+    ]
+
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE}
+    }
+
+admin.site.register(Game, GameAdmin)
+
+
