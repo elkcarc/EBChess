80007cb484960ac6a48029727e86c87a0fe5c17c
Peyton Hazama <peytonhazama@gmail.com>
2020-04-06 20:34:47 -0400

    testing html and css from db

--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Game
+
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("僕はミームに愛をする")
+    return render(request=request, 
+                  template_name="main/home.html",
+                  context={"games": Game.objects.all})
+
+                  
